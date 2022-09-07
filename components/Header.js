4df5f246fbd7e82d00efc2a731ebce2893d5b647
rbc42fl <83221715+rbc42fl@users.noreply.github.com>
2022-09-06 22:26:27 -0400

    Add search component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { useRouter } from 'next/router';
-
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 export default function Header() {
   const router = useRouter();
   return (
@@ -29,6 +29,16 @@ export default function Header() {
       </div>
 
       {/*  // middle//// */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
 
       {/* // right///  */}
 
