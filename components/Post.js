dc0350757877a1409bcc63c50163a093bc15d9c0
rbc42fl <83221715+rbc42fl@users.noreply.github.com>
2022-09-10 12:51:50 -0400

    create header and img section of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,24 @@
+import { PlusIcon } from '@heroicons/react/24/outline';
+
 export default function Post({ img, userImg, caption, username, id }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+
+      <div className="flex items-center">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1 ">{username}</p>
+        <PlusIcon className="h-5" />
+        <PlusIcon className="h-5" />
+        <PlusIcon className="h-5" />
+        <p className="mr-4">{caption}</p>
+      </div>
+      {/* post image */}
+      <img className="object-cover max-w-full" src={img} alt="img" />
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -4,18 +4,18 @@ export default function Posts() {
   const posts = [
     {
       id: '1',
-      username: 'codewithsahand',
+      username: 'alotofcode',
       userImg:
-        'https://staticic.skill.com/uploads/users/35031760/user-image-large.jpg',
-      img: 'https://images.unsplash.com/photo-1433838552652-f9a46b332c40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+      img: 'https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
       caption: 'Thanks for your post',
     },
     {
       id: '2',
       username: 'alotofcode',
       userImg:
-        'https://staticic.skill.com/uploads/users/35031760/user-image -large.jpg',
-      img: 'https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+      img: 'https://images.unsplash.com/photo-1660924197568-440cb5baa08e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60',
       caption: 'Thanks for your post',
     },
   ];
@@ -26,8 +26,8 @@ export default function Posts() {
           key={post.id}
           id={post.id}
           username={post.username}
-          userImg={post.profileImg}
-          img={post.image}
+          userImg={post.userImg}
+          img={post.img}
           caption={post.caption}
         />
       ))}
