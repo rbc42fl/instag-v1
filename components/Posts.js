0cf0a75a7ea7c700c55fdf59d451a11e9410289c
rbc42fl <83221715+rbc42fl@users.noreply.github.com>
2022-09-22 18:59:49 -0400

    get the post from the database and show it in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,34 +1,29 @@
+import { useEffect, useState } from 'react';
 import Post from './Post';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function Posts() {
-  const posts = [
-    {
-      id: '1',
-      username: 'alotofcode',
-      userImg:
-        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-      img: 'https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      caption: 'Thanks for your post',
-    },
-    {
-      id: '2',
-      username: 'alotofcode',
-      userImg:
-        'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-      img: 'https://images.unsplash.com/photo-1660924197568-440cb5baa08e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60',
-      caption: 'Thanks for your post',
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, [db]);
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
--- components/UploadModal.js
+++ components/UploadModal.js
@@ -4,10 +4,48 @@ import Modal from 'react-modal';
 import { modalState } from '../atom/modalAtom';
 
 import { CameraIcon } from '@heroicons/react/24/outline';
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from 'firebase/firestore';
+import { db, storage } from '../firebase';
+import { useSession } from 'next-auth/react';
+import { ref, uploadString, getDownloadURL } from 'firebase/storage';
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { data: session } = useSession();
+  //const [currentUser] = useRecoilState(userState);
+
+  async function uploadPost() {
+    if (loading) return;
+
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      caption: captionRef.current.value,
+      username: session.user.username,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    await uploadString(imageRef, selectedFile, 'data_url').then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, 'posts', docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  }
 
   function addImageToPost(event) {
     const reader = new FileReader();
@@ -21,6 +59,7 @@ export default function UploadModal() {
   }
 
   const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
   return (
     <>
       <div>
@@ -59,10 +98,11 @@ export default function UploadModal() {
                 maxLength="150"
                 placeholder="Please enter your caption..."
                 className="m-4 border-none text-center w-full focus:ring-0"
+                ref={captionRef}
               />
               <button
-                // disabled={!selectedFile || loading}
-                // onClick={uploadPost}
+                disabled={!selectedFile || loading}
+                onClick={uploadPost}
                 className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
               >
                 Upload Post
