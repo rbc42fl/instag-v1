405418087df2e6b9e9e40096353e3a9af1d8ba81
rbc42fl <83221715+rbc42fl@users.noreply.github.com>
2022-09-23 21:18:18 -0400

    add like post number

--- components/Post.js
+++ components/Post.js
@@ -125,6 +125,9 @@ export default function Post({ img, userImg, caption, username, id }) {
       {/* Post Comments section /////////////////////////*/}
 
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
