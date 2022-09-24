72e3421375363188a62f31dd8c6ff8c19095b20e
rbc42fl <83221715+rbc42fl@users.noreply.github.com>
2022-09-23 20:55:43 -0400

    copied suggestion file

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,10 +1,9 @@
 import minifaker from 'minifaker';
-
-import { useState, useEffect } from 'react';
+import 'minifaker/locales/en';
+import { useEffect, useState } from 'react';
 
 export default function Suggestions() {
   const [suggestions, setSuggestions] = useState([]);
-
   useEffect(() => {
     const suggestions = minifaker.array(5, (i) => ({
       username: minifaker.username({ locale: 'en' }).toLowerCase(),
@@ -13,17 +12,18 @@ export default function Suggestions() {
     }));
     setSuggestions(suggestions);
   }, []);
-
   return (
     <div className="mt-4 ml-10">
       <div className="flex justify-between mb-5 text-sm">
-        <h3 className="font-bold text-gray-400">Suggestions for you!</h3>
-        <button className="text-gray-600 font-semibold">See All</button>
+        <h3 className="font-bold text-gray-400">Suggestion for you</h3>
+        <button className="text-gray-600 font-semibold">See all</button>
       </div>
       {suggestions.map((suggestion) => (
-        <div className="flex items-center justify-between mt-3">
+        <div
+          key={suggestion.id}
+          className="flex items-center justify-between mt-3"
+        >
           <img
-            key={suggestion.id}
             className="h-10 rounded-full border p-[2px]"
             src={`https://i.pravatar.cc/150?img=${Math.ceil(
               Math.random() * 70
